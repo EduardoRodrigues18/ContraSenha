b6fead01326b76b83f9b2936e630c071276c19f6
Eduardo R. dos Santos <91218824+EduardoRodrigues18@users.noreply.github.com>
2025-01-20 11:32:13 -0300

    ajustes no exe

--- Project/run.js
+++ Project/run.js
@@ -1,18 +1,16 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 // Comando que você deseja executar
-const comando = "nodemon ./app.js";
+const comando = "cmd.exe";
+const args = ["/c", "start", "/min", "cmd.exe", "/c", "nodemon ./app.js"];
 
-// Executa o comando no PowerShell
-exec(comando, { shell: "cmd.exe" }, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Erro: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Output:\n${stdout}`);
-  });
-  
+// Inicia o comando no cmd com a opção detached
+const processo = spawn(comando, args, {
+  detached: true,
+  stdio: "ignore", // Ignora o stdout e stderr do processo filho
+});
+
+// Encerra o processo pai (Node.js) e mantém o cmd rodando
+processo.unref();
+
+console.log("Processo iniciado em segundo plano.");
